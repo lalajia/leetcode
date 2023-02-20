967493620f4df7bc45f161ddcf6bd43f611ca1b4
Selena <selenazheng3@gmail.com>
2023-02-20 13:40:20 -0800

    Maximum Depth of Binary Tree

---
+++ Tree/SameTree/Solution.java
@@ -0,0 +1,57 @@
+package SameTree;
+
+/**
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or
+ * not.
+ *
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have
+ * the same value.
+ */
+public class Solution {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static void main(String args[]) {
+    TreeNode rootOne = new TreeNode(3);
+//    rootOne.left = new TreeNode(9);
+//    rootOne.right = new TreeNode(20);
+    TreeNode rootTwo = new TreeNode(3);
+//    rootTwo.left = new TreeNode(9);
+//    rootTwo.right = new TreeNode(20);
+    System.out.println(isSameTree(rootOne, rootTwo));
+  }
+
+  public static boolean isSameTree(TreeNode p, TreeNode q) {
+    //对比到头的时候需要两者相等
+    if (p == null && q == null)
+      return true;
+    if (p == null || q == null)
+      return false;
+    //compare value
+    if (p.val != q.val)
+      return false;
+    Boolean typeLeft = isSameTree(p.left, q.left);
+    Boolean typeRight = isSameTree(p.right, q.right);
+    if (typeRight && typeLeft) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
